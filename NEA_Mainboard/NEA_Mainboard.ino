e37dd34ac52831e52d0af218b4e2e4afdb27a82c
tanve3r <tanve3r@gmail.com>
2024-04-20 21:38:26 +0530

    fixed person count logic

--- NEA_Mainboard/NEA_Mainboard.ino
+++ NEA_Mainboard/NEA_Mainboard.ino
@@ -3,15 +3,25 @@
 #include "lcdgfx.h"
 #include "lcdgfx_gui.h"
 
-
 #define   MESH_PREFIX     "whateverYouLike"
 #define   MESH_PASSWORD   "somethingSneaky"
 #define   MESH_PORT       5555
-
+const int relay1pin = 33;  // the number of the LED pin
+const int relay2pin = 32;  // the number of the LED pin
 
 Scheduler userScheduler; // to control your personal task
 painlessMesh  mesh;
 DisplaySH1107_128x64_I2C display(-1);
+
+static int PersonCount = 0;
+static int detetion1_set = 0;
+static int detection2_set = 0;
+static int decrementlogic = 0;
+static int incrementlogic = 0;
+static int detection1_pervious = 0;
+static int detection2_pervious = 0;
+static int motion1 =0;
+static int motion2 = 0;
 
 // User stub
 void sendMessage() ; // Prototype so PlatformIO doesn't complain
@@ -58,45 +68,128 @@ void receivedCallback(uint32_t from, String &msg)
 
   if( (Temp1 != "null") || (Hum1 != "null") || (Lux1 != "null") || (Motion1 != "null") )
   {
-    sprintf(buffer, " Temp1:%s", Temp1);
+    sprintf(buffer, " Temp1:%03d", Temp1.toInt());
     display.printFixed(0,  0, buffer, STYLE_NORMAL);
 
-    sprintf(buffer, " Humi1:%s", Hum1);
+    sprintf(buffer, " Humi1:%03d", Hum1.toInt());
     display.printFixed(0,  8, buffer, STYLE_NORMAL);
 
-    sprintf(buffer, " Lux1 :%s", Lux1);
+    sprintf(buffer, " Lux1 :%03d", Lux1.toInt());
     display.printFixed(0,  16, buffer, STYLE_NORMAL);
-
-    sprintf(buffer, " Mot1 :%s", Motion1);
-    display.printFixed(0,  24, buffer, STYLE_NORMAL);
+    
+    motion1 = Motion1.toInt();
+    if(Motion1.toInt() < 10)
+    {
+      display.printFixed(0,  24, " Mot1 : 1", STYLE_NORMAL);
+      detetion1_set = 1;
+    }
+    else
+    {
+      display.printFixed(0,  24, " Mot1 : 0", STYLE_NORMAL);
+    }
+    sprintf(buffer, " USD1 :%03d", Motion1.toInt());
+    display.printFixed(0,  32, buffer, STYLE_NORMAL);
+    // Serial.printf("Distance1: %s",Motion1);
+    // Serial.println(Motion1);
   }
 
   if( (Temp2 != "null") || (Hum2 != "null") || (Lux2 != "null") || (Motion2 != "null") )
   {
-    sprintf(buffer, " Temp2:%s", Temp2);
-    display.printFixed(0,  32, buffer, STYLE_NORMAL);
-
-    sprintf(buffer, " Humi2:%s", Hum2);
+    sprintf(buffer, " Temp2:%03d", Temp2.toInt());
     display.printFixed(0,  40, buffer, STYLE_NORMAL);
 
-    sprintf(buffer, "Lux2 :%s", Lux2);
+    sprintf(buffer, "Humi2:%03d", Hum2.toInt());
     display.printFixed(65,  0, buffer, STYLE_NORMAL);
 
-    sprintf(buffer, "Mot2 :%s", Motion2);
+    sprintf(buffer, "Lux2 :%03d", Lux2.toInt());
     display.printFixed(65,  8, buffer, STYLE_NORMAL);
+    
+    motion2 = Motion2.toInt();
+    if(Motion2.toInt() < 10)
+    {
+      display.printFixed(65,  16, "Mot2 : 1", STYLE_NORMAL);
+      detection2_set = 1;
+    }
+    else
+    {
+      display.printFixed(65,  16, "Mot2 : 0", STYLE_NORMAL);
+    }
+
+    sprintf(buffer, "USD2 :%03d", Motion2.toInt());
+    display.printFixed(65,  24, buffer, STYLE_NORMAL);
+    // Serial.printf("  Distance2: ");
+    // Serial.println(Motion2);
   }
   
   if( (Temp3 != "null") || (Hum3 != "null") || (Lux3 != "null") )
   {
-    sprintf(buffer, "Temp3:%s", Temp3);
-    display.printFixed(65,  16, buffer, STYLE_NORMAL);
-
-    sprintf(buffer, "Humi3:%s", Hum3);
-    display.printFixed(65,  24, buffer, STYLE_NORMAL);
-
-    sprintf(buffer, "Lux3 :%s", Lux3);
+    sprintf(buffer, "Temp3:%03d", Temp3.toInt());
     display.printFixed(65,  32, buffer, STYLE_NORMAL);
+
+    sprintf(buffer, "Humi3:%03d", Hum3.toInt());
+    display.printFixed(65,  40, buffer, STYLE_NORMAL);
+
+    sprintf(buffer, "Lux3 :%03d", Lux3.toInt());
+    display.printFixed(65,  48, buffer, STYLE_NORMAL);
+    // Serial.printf("Lux3: ");
+    // Serial.println(Lux3);
   }
+
+  // Serial.printf("d1:%d d2: %d \n",detetion1_set,detection2_set);
+
+  if((detetion1_set == 1) && (detection2_set == 0))
+  {
+    incrementlogic = 1;
+  }
+
+
+  if(incrementlogic == 1)
+  {
+    if((detection2_set == 1)&&(detection2_pervious == 0))
+    {
+      PersonCount ++;
+    }
+  }
+
+  if((detection2_set == 1) && (detetion1_set ==0))
+  {
+    decrementlogic = 1;
+  }
+
+  if(decrementlogic == 1)
+  {
+    if((detetion1_set == 1)&&(detection1_pervious == 0))
+    {
+      if(PersonCount > 0)
+      PersonCount --;
+    }
+  }
+
+  if(detetion1_set==1 && detection2_set==1)
+  {
+    if(motion1 > 10 && motion2 > 10)
+    {
+      detetion1_set = 0;
+      detection2_set = 0;
+      incrementlogic = 0;
+      decrementlogic = 0;
+    }
+  }
+
+  sprintf(buffer, " PerCo:%03d", PersonCount);
+  display.printFixed(0,  48, buffer, STYLE_NORMAL);
+  // Serial.printf("pc:%d dec:%d inc:%d\n", PersonCount,decrementlogic,incrementlogic);
+  detection1_pervious = detetion1_set;
+  detection2_pervious = detection2_set;
+  if(PersonCount > 0)
+  {
+    digitalWrite(relay1pin, LOW);
+  }
+  else
+  {
+    digitalWrite(relay1pin, HIGH);
+  }
+  
 }
 
 void newConnectionCallback(uint32_t nodeId) {
@@ -130,17 +223,23 @@ void setup() {
 
   display.setFixedFont(ssd1306xled_font6x8);
   display.clear();
-  display.printFixed(0,  0, " Temp1=0", STYLE_NORMAL);
-  display.printFixed(0,  8, " Humi1=0", STYLE_NORMAL);
-  display.printFixed(0,  16, " Lux1 =0", STYLE_NORMAL);
-  display.printFixed(0,  24, " Mot1 =0", STYLE_NORMAL);
-  display.printFixed(0,  32, " Temp2=0", STYLE_NORMAL);
-  display.printFixed(0,  40, " Humi2=0", STYLE_NORMAL);
-  display.printFixed(65,  0, "Lux2 =0", STYLE_NORMAL);
-  display.printFixed(65,  8, "Mot2 =0", STYLE_NORMAL);
-  display.printFixed(65,  16, "Temp3=0", STYLE_NORMAL);
-  display.printFixed(65,  24, "Humi3=0", STYLE_NORMAL);
-  display.printFixed(65,  32, "Lux3 =0", STYLE_NORMAL);
+  display.printFixed(0,  0, " Temp1:0", STYLE_NORMAL);
+  display.printFixed(0,  8, " Humi1:0", STYLE_NORMAL);
+  display.printFixed(0,  16, " Lux1 :0", STYLE_NORMAL);
+  display.printFixed(0,  24, " Mot1 :0", STYLE_NORMAL);
+  display.printFixed(0,  32, " USD1 :0", STYLE_NORMAL);
+  display.printFixed(0,  40, " Temp2:0", STYLE_NORMAL);
+  display.printFixed(0,  48, " PerCo:0", STYLE_NORMAL);
+  display.printFixed(65,  0, "Humi2:0", STYLE_NORMAL);
+  display.printFixed(65,  8, "Lux2 :0", STYLE_NORMAL);
+  display.printFixed(65,  16, "Mot2 :0", STYLE_NORMAL);
+  display.printFixed(65,  24, "USD2 :0", STYLE_NORMAL);
+  display.printFixed(65,  32, "Temp3:0", STYLE_NORMAL);
+  display.printFixed(65,  40, "Humi3:0", STYLE_NORMAL);
+  display.printFixed(65,  48, "Lux3 :0", STYLE_NORMAL);
+  pinMode(relay1pin, OUTPUT);
+  pinMode(relay2pin, OUTPUT);
+  digitalWrite(relay1pin, HIGH);
 }
 
 void loop() {
